ea2a1c9aee06d828a056fda182ab8c62a008f383
lostsavage69 <97002545+lostsavage69@users.noreply.github.com>
2023-01-16 22:52:11 +0000

    Add files via upload

---
+++ links.js
@@ -0,0 +1,51 @@
+var username1 = "Lex";
+var hashtag1 = "1273";
+
+var username2 = "Raven";
+var hashtag2 = "800";
+
+var username3 = "Shnoodle";
+var hashtag3 = "8201";
+
+var username4 = "Hazmat";
+var hashtag4 = "rizz";
+
+var username5 = "Oopslol";
+var hashtag5 = "oops";
+
+var username6 = "InFuZed";
+var hashtag6 = "7126";
+
+var username7 = "Hazmats kitty";
+var hashtag7 = "8186";
+
+var link1 = "https://tracker.gg/valorant/profile/riot/" + username1 + "%23" + hashtag1 + "/overview";
+var link2 = "https://tracker.gg/valorant/profile/riot/" + username2 + "%23" + hashtag2 + "/overview";
+var link3 = "https://tracker.gg/valorant/profile/riot/" + username3 + "%23" + hashtag3 + "/overview";
+var link4 = "https://tracker.gg/valorant/profile/riot/" + username4 + "%23" + hashtag4 + "/overview";
+var link5 = "https://tracker.gg/valorant/profile/riot/" + username5 + "%23" + hashtag5 + "/overview";
+var link6 = "https://tracker.gg/valorant/profile/riot/" + username6 + "%23" + hashtag6 + "/overview";
+var link7 = "https://tracker.gg/valorant/profile/riot/" + username7 + "%23" + hashtag7 + "/overview";
+
+var a = document.createElement("a");
+document.write("<p>" +username1+":"+"<a href='"+link1+"'> Here </a><br>");
+
+var a = document.createElement("a");
+document.write("<p>" +username2+":"+"<a href='"+link2+"'> Here </a><br>");
+
+var a = document.createElement("a");
+document.write("<p>" +username3+":"+"<a href='"+link3+"'> Here </a><br>");
+
+var a = document.createElement("a");
+document.write("<p>" +username4+":"+"<a href='"+link4+"'> Here </a><br>");
+
+var a = document.createElement("a");
+document.write("<p>" +username5+":"+"<a href='"+link5+"'> Here </a><br>");
+
+var a = document.createElement("a");
+document.write("<p>" +username6+":"+"<a href='"+link6+"'> Here </a><br>");
+
+var a = document.createElement("a");
+document.write("<p>" +username7+":"+"<a href='"+link7+"'> Here </a><br>");
+
+
